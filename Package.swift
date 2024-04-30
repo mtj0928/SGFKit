1a111bc032578d01d0440ebd6a1d0946c2ac43c0
mtj0928 <mtj0928@users.noreply.github.com>
2024-04-30 09:30:48 +0900

    Clean Package.swift

--- Package.swift
+++ Package.swift
@@ -1,24 +1,14 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SGFKit",
-    platforms: [.iOS(.v16), .macOS(.v13), .visionOS(.v1), .watchOS(.v9), .tvOS(.v16)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SGFKit",
-            targets: ["SGFKit"]),
+        .library(name: "SGFKit", targets: ["SGFKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SGFKit"),
-        .testTarget(
-            name: "SGFKitTests",
-            dependencies: ["SGFKit"]),
+        .target(name: "SGFKit"),
+        .testTarget(name: "SGFKitTests", dependencies: ["SGFKit"]),
     ]
 )
