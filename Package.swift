0e2e7d004912ec660a66722501bc85c9d2ccdb8d
mtj0928 <mtj0928@users.noreply.github.com>
2024-05-03 19:13:06 +0900

    Add swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -7,6 +7,9 @@ let package = Package(
     products: [
         .library(name: "SGFKit", targets: ["SGFKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+    ],
     targets: [
         .target(name: "SGFKit"),
         .testTarget(name: "SGFKitTests", dependencies: ["SGFKit"]),
